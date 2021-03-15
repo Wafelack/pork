c4d45c5464eb446df56387995725e1a25a59a87b
Wafelack <wafelack@protonmail.com>
2021-03-15 17:35:50 +0100

    legal: Added license notice to files.

--- src/password.rs
+++ src/password.rs
@@ -1,3 +1,20 @@
+/*
+ *     This file is part of rad.
+ *
+ *   rad is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   rad is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *
+ *  along with rad.  If not, see <https://www.gnu.org/licenses/>.
+ */
 use crate::*;
 use pwhash::{md5_crypt, sha256_crypt, sha512_crypt};
 use std::fs;
